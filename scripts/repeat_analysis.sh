ebbd411978a013b1367643045dffc60ac295ebbb
Aswin S Soman <ceglab3@gmail.com>
2026-02-12 13:57:31 +0530

    Update repeat_analysis.sh

--- scripts/repeat_analysis.sh
+++ scripts/repeat_analysis.sh
@@ -1,14 +1,14 @@
 
 
 cd /media/aswin/SCFR/SCFR-main
-time for seq in $(find Length_threshold_PCA_kmeans/ -mindepth 3 -name "*_gt1000.fasta" -type f)
+time for seq in $(find Length_threshold_PCA_kmeans/ -mindepth 3 -name "*_gt1000.fasta" -type f | xargs readlink -f)
 do
 mkdir -p repeat_masker/"$species"
-species=$(echo $seq | cut -f2 -d "/")
+species=$(echo $seq | cut -f7 -d "/")
+sp=$(grep -i "$species" /media/aswin/SCFR/SCFR-main/genome_reports/species_names | awk '{print$2}' | tr "_" " ")
 echo ">"$species
-#sp=$(grep -i "$species" /media/aswin/SCFR/SCFR-main/genome_reports/species_names | awk '{print$1}' | tr "_" " ")
 cd repeat_masker
-time /media/aswin/programs/RepeatMasker-4.1.7/RepeatMasker/RepeatMasker -pa 8 -a -s -u -gff -html -species "$species" -dir $species -xsmall $seq
-unset species
+time /media/aswin/programs/RepeatMasker-4.1.7/RepeatMasker/RepeatMasker -pa 8 -a -s -u -gff -html -species "$sp" -dir $species -xsmall $seq
+unset species sp
 cd /media/aswin/SCFR/SCFR-main
 done
