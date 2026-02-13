a68b8785d7716bf66b6d74434cb79f3b7e279384
Aswin S Soman <ceglab3@gmail.com>
2026-02-14 02:00:09 +0530

    Update PCA_analysis.sh

--- scripts/PCA_analysis.sh
+++ scripts/PCA_analysis.sh
@@ -205,7 +205,7 @@ done
 
 #Run repeat masker (ran for 3 species (chimpanzee, gorilla, human) using species library, for rest hominidiae is used as species)
 cd /media/aswin/SCFR/SCFR-main
-time for seq in $(find Length_threshold_PCA_kmeans/ -mindepth 3 -name "*_gt1000.fasta" -type f | xargs readlink -f | egrep "gibbon|borangutan|bonobo|sorangutan")
+time for seq in $(find Length_threshold_PCA_kmeans/ -mindepth 3 -name "*_gt1000.fasta" -type f | xargs readlink -f)
 do
 mkdir -p repeat_masker/"$species"
 species=$(echo $seq | cut -f7 -d "/")
@@ -214,6 +214,21 @@ echo ">"$species
 cd repeat_masker
 time /media/aswin/programs/RepeatMasker-4.1.7/RepeatMasker/RepeatMasker -pa 8 -a -s -u -gff -html -species "Hominidae" -dir $species -xsmall $seq
 unset species sp
+cd /media/aswin/SCFR/SCFR-main
+done
+
+#######################################################################################################################################################################################################################################################################################################
+#Get composition
+
+cd /media/aswin/SCFR/SCFR-main
+for seq in $(find Length_threshold_PCA_kmeans/ -mindepth 3 -maxdepth 3 -name "*gt1000.fasta" -type f | xargs readlink -f)
+do
+species=$(echo $seq | cut -f7 -d "/")
+sp=$(grep -i "$species" /media/aswin/SCFR/SCFR-main/genome_reports/species_names | awk '{print$2}' | tr "_" " ")
+path=$(echo $seq | awk -F "/" '!($NF="")'  OFS="/")
+cd $path
+echo ">"$species
+/media/aswin/SCFR/SCFR-main/my_scripts/exon_shadow/get_composition.sh $seq > "$species"_gt1000_composition.tsv
 cd /media/aswin/SCFR/SCFR-main
 done
 
@@ -230,6 +245,7 @@ coding_status=$(readlink -f /media/aswin/SCFR/SCFR-main/Length_threshold_PCA_kme
 repeat_class=$(readlink -f /media/aswin/SCFR/SCFR-main/repeat_masker/"$species"/"$species"_gt1000.fasta.out)
 python3 /media/aswin/SCFR/SCFR-main/my_scripts/PCA/fastaout_to_tsv.py $repeat_class /media/aswin/SCFR/SCFR-main/repeat_masker/"$species"/"$species"_gt1000_pca_input.out
 repeat_input=$(readlink -f /media/aswin/SCFR/SCFR-main/repeat_masker/"$species"/"$species"_gt1000_pca_input.out)
+gc=$(readlink -f /media/aswin/SCFR/SCFR-main/Length_threshold_PCA_kmeans/"$species"/gt1000/"$species"_gt1000_composition.tsv)
 for lenbin in 2500_5000 5000_7500 7500_10000 gt10000
 do
 echo " -"$lenbin
@@ -239,6 +255,7 @@ Rscript /media/aswin/SCFR/SCFR-main/my_scripts/PCA/plot_color_by_clade.R . .
 Rscript /media/aswin/SCFR/SCFR-main/my_scripts/PCA/plot_by_cluster.R . .
 Rscript /media/aswin/SCFR/SCFR-main/my_scripts/PCA/plot_by_coding_status.R . . $coding_status
 Rscript /media/aswin/SCFR/SCFR-main/my_scripts/PCA/plot_by_repeat_family.R . . $repeat_input
+Rscript /media/aswin/SCFR/SCFR-main/my_scripts/PCA/plot_by_GC_content.R . . $gc
 cd ../
 done
 unset lenbin coding_status repeat_class repeat_input
@@ -255,6 +272,7 @@ coding_status=$(readlink -f /media/aswin/SCFR/SCFR-main/Length_threshold_PCA_kme
 repeat_class=$(readlink -f /media/aswin/SCFR/SCFR-main/repeat_masker/"$species"/"$species"_gt1000.fasta.out)
 python3 /media/aswin/SCFR/SCFR-main/my_scripts/PCA/fastaout_to_tsv.py $repeat_class /media/aswin/SCFR/SCFR-main/repeat_masker/"$species"/"$species"_gt1000_pca_input.out
 repeat_input=$(readlink -f /media/aswin/SCFR/SCFR-main/repeat_masker/"$species"/"$species"_gt1000_pca_input.out)
+gc=$(readlink -f /media/aswin/SCFR/SCFR-main/Length_threshold_PCA_kmeans/"$species"/gt1000/"$species"_gt1000_composition.tsv)
 for lenbin in gt2500 gt5000 gt7500 gt10000
 do
 echo " -"$lenbin
@@ -264,6 +282,7 @@ Rscript /media/aswin/SCFR/SCFR-main/my_scripts/PCA/plot_color_by_clade.R . .
 Rscript /media/aswin/SCFR/SCFR-main/my_scripts/PCA/plot_by_cluster.R . .
 Rscript /media/aswin/SCFR/SCFR-main/my_scripts/PCA/plot_by_coding_status.R . . $coding_status
 Rscript /media/aswin/SCFR/SCFR-main/my_scripts/PCA/plot_by_repeat_family.R . . $repeat_input
+Rscript /media/aswin/SCFR/SCFR-main/my_scripts/PCA/plot_by_GC_content.R . . $gc
 cd ../
 done
 unset lenbin coding_status repeat_class repeat_input
